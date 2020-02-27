d186d521037690a1263e25644376a9c9f04544c7
syreal17 <ltjones@acm.org>
2020-02-27 16:56:57 -0500

    +[basic auth for entire site]

--- atlas-web-app/config/policies.js
+++ atlas-web-app/config/policies.js
@@ -8,6 +8,19 @@
  * https://sailsjs.com/docs/concepts/policies
  */
 
+// Authentication module.
+const auth = require('http-auth');
+const basic = auth.basic({
+    realm: "CONIX"
+  }, (username, password, callback) => {
+    // Custom authentication
+    // Use callback(error) if you want to throw async error.
+    callback(username === "conix" && password === "conix");
+  }
+);
+const auth_connect = require('http-auth-connect');
+
+
 module.exports.policies = {
 
   /***************************************************************************
@@ -17,6 +30,7 @@ module.exports.policies = {
   *                                                                          *
   ***************************************************************************/
 
-  '*': 'isLocal',
+  '*': auth_connect(basic),
+  //'*': 'isLocal',
 
 };
