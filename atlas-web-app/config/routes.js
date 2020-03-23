8e96e32193fe0688f82f2182172a6a2f899135d6
syreal17 <ltjones@acm.org>
2020-03-23 16:38:18 -0400

    Remove archaic file system esque nav

--- atlas-web-app/config/routes.js
+++ atlas-web-app/config/routes.js
@@ -20,15 +20,15 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': { view: 'pages/landing-geo-loc-near' },
+  '/landing-geo-loc-near': { view: 'pages/landing-geo-loc-near' },
   '/home': { view: 'pages/homepage' },
   '/portal': { view: 'pages/portal' },
   '/about': { view: 'pages/about' },
   '/manage': { view: 'pages/manage' },
   '/create-record-form': { view: 'pages/create-record-form' },
   '/search-records-form': { view: 'pages/search-records-form' },
-  '/update-record-form': { view: 'pages/update-record-form'},
+  '/update-record-form': { view: 'pages/update-record-form' },
   '/delete-record-form': { view: 'pages/delete-record-form' },
-  '/landing-geo-loc-near': { view: 'pages/landing-geo-loc-near' },
 
   'GET /redirect/:id': { action: 'record/redirect-id' },
   'GET /lookup/geo': { action: 'record/geo-radius' },
