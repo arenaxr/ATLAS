f33f4cdce6bc1929a0c53604ba0f26a50f422468
syreal17 <ltjones@acm.org>
2020-04-08 14:30:21 -0400

    Wrote script to autogen 20 records

---
+++ testing/init.many.rgtsq.records.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# NOTE:_On Adding New Records to this Script_
+# running this script after adding new records is fine because nothing w
+# the same UUID will be added again!
+
+#------------------------------------------------------------------------------
+#test scenes for Luke
+curl --basic -u conix 'http://localhost:9001/record/create?id=836727c6-aec1-43bc-91c2-c473650cce8d&name=RGTSQ1&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=f22e9ab0-57e0-4293-98ff-4b0c56f600ca&name=RGTSQ2&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=ae06bf6b-00eb-43da-8fec-526d1e09f819&name=RGTSQ3&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=fbb825ac-5d8f-4f81-9071-6f0a498f167b&name=RGTSQ4&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=54ced66e-728d-4f3b-8ea3-7c74892e6f66&name=RGTSQ5&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=1b65162c-4bd5-4be2-96ca-ff20f78238e0&name=RGTSQ6&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=aba84f2e-8142-49d5-941b-6f0ad4c1cf08&name=RGTSQ7&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=513a632f-1b22-4314-a162-de6373d5aaa2&name=RGTSQ8&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=9c3bdadd-3294-4682-b46a-a60ea7bcbbe3&name=RGTSQ9&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=8801af7b-c19e-49d5-a993-5445eb2224a9&name=RGTSQ10&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=607c9268-e274-49b3-ab58-773ddced4c2d&name=RGTSQ11&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=83a1e270-c0ff-42af-901a-7678413f2384&name=RGTSQ12&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=4a92db44-8030-4bc1-9b17-ca505e903893&name=RGTSQ13&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=7323f107-8491-45ad-9887-569d4f92b0ba&name=RGTSQ14&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=0cc6ce4c-7488-4afc-866d-9e991686fd1e&name=RGTSQ15&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=42aaffaa-e8b1-48ab-ac0d-02b79b237606&name=RGTSQ16&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=03302ff5-fbf8-4f9a-8404-53e9661ea46d&name=RGTSQ17&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=40c26819-1d4e-44e1-acd5-27549160ab7a&name=RGTSQ18&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=08908663-27f1-4344-aa51-fb9303e48f55&name=RGTSQ19&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
+curl --basic -u conix 'http://localhost:9001/record/create?id=6369e731-4ffe-4e69-93f0-898e8dce955e&name=RGTSQ20&lat=40.438763&long=-79.892235&ele=200&url=https://www.ltsyrealjones.com'
