89f3d6313a97ee6613209c97fefaf790dc561e81
Ivan Liang <r33t@cmu.edu>
2020-07-27 14:13:06 -0400

    Format passport controller file

--- atlas-web-app/api/controllers/PassportController.js
+++ atlas-web-app/api/controllers/PassportController.js
@@ -9,15 +9,15 @@ var passport = require('passport');
 
 module.exports = {
 
-  googleAuth: function(req, res) {
-    passport.authenticate('google', { scope: ['email', 'profile'] })(req, res);
+  googleAuth: function (req, res) {
+    passport.authenticate('google', {scope: ['email', 'profile']})(req, res);
   },
 
-  googleCallback: function(req, res, next) {
-    passport.authenticate('google', function(err, user) {
-      if(err) {
+  googleCallback: function (req, res, next) {
+    passport.authenticate('google', function (err, user) {
+      if (err) {
         // redirect to login page
-        console.log('google callback error: '+err);
+        console.log('google callback error: ' + err);
       } else {
         console.log('google credentials');
         console.log(user);
